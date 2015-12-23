d2b787b52eb268103ca8bc425bd652e355701b85
Mathias Ertl <mati@er.tl>
2015-12-23 18:32:56 +0100

    set more secure defaults (force local setting of SECRET_KEY

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u-i579!^+67ij-173v8ycbfb=!cs9l=#2lxce=#0#f%u7*h%1%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
